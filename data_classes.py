42c36b88b26fd6bee960cadd876f6baaaea4d164
703235761@genpact.com <703235761@genpact.com>
2021-07-26 14:22:03 +0530

    changes

---
+++ data_classes.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+import math
+
+@dataclass
+class FDDBExample:
+    fold:int
+    image_path:str
+    major_axis_radius:float
+    minor_axis_radius:float
+    angle:float
+    center_x:float
+    center_y:float
+    
+    @property
+    def center(self):
+        return (self.center_x, self.center_y)
+    
+    @property
+    def axes(self):
+        #return (int(self.major_axis_radius*2), int(self.minor_axis_radius*2))
+        return (self.major_axis_radius*2, self.minor_axis_radius*2)
+    
+    def cv2_ellipse_box(self):
+        return (self.center, self.axes, math.degrees(self.angle))
+    
+    def cv2_rectangle_box(self):
+        radians = self.angle
+        radians90 = radians + math.pi / 2
+        radiusX, radiusY = self.major_axis_radius, self.minor_axis_radius
+        ux = radiusX * math.cos(radians)
+        uy = radiusX * math.sin(radians)
+        vx = radiusY * math.cos(radians90)
+        vy = radiusY * math.sin(radians90)
+        width = math.sqrt(ux * ux + vx * vx) * 2
+        height = math.sqrt(uy * uy + vy * vy) * 2
+        x1, y1 = int(self.center_x - (width / 2)), int(self.center_y - (height / 2))
+        x2, y2 = int(self.center_x + (width / 2)), int(self.center_y + (height / 2))
+        return (x1, y1), (x2, y2)
+        
+        
+    
+    
---
+++ feature_engineering.py
@@ -0,0 +1,102 @@
+import cv2
+import numpy as np
+import os
+from os import path
+from PIL import Image
+from object_detection.utils import dataset_util, label_map_util
+if __package__:
+    from .data_classes import FDDBExample
+else:
+    from data_classes import FDDBExample
+
+def read_folds(fold_start=1, fold_end=1):
+    for i in range(fold_start,fold_end+1):
+        fpath = path.join('FDDB-folds', f'FDDB-fold-{str(i).zfill(2)}-ellipseList.txt')
+        with open(fpath, 'r') as f:
+            img_path = None
+            n_faces = None
+            for line in f:
+                line = line.strip()
+                if not img_path:
+                    img_path = line+'.jpg'
+                elif not n_faces:
+                    n_faces = int(line)
+                else:
+                    #bounding box
+                    n_faces -=1
+                    ellipse_def = line[:line.index('  ')].split(' ')
+                    ellipse_def = [float(item) for item in ellipse_def]
+                    yield FDDBExample(
+                        fold= i,\
+                        image_path= img_path,\
+                        major_axis_radius= ellipse_def[0],\
+                        minor_axis_radius= ellipse_def[1],\
+                        angle=ellipse_def[2],\
+                        center_x=ellipse_def[3],\
+                        center_y= ellipse_def[4])
+                    if n_faces==0:
+                        #reset
+                        img_path, n_faces = None, None
+                        
+def create_tf_example(group, path):
+    with tf.gfile.GFile(os.path.join(path, '{}'.format(group.filename)), 'rb') as fid:
+        encoded_jpg = fid.read()
+    encoded_jpg_io = io.BytesIO(encoded_jpg)
+    image = Image.open(encoded_jpg_io)
+    width, height = image.size
+
+    filename = group.filename.encode('utf8')
+    image_format = b'jpg'
+    xmins = []
+    xmaxs = []
+    ymins = []
+    ymaxs = []
+    classes_text = []
+    classes = []
+
+    for index, row in group.object.iterrows():
+        xmins.append(row['xmin'] / width)
+        xmaxs.append(row['xmax'] / width)
+        ymins.append(row['ymin'] / height)
+        ymaxs.append(row['ymax'] / height)
+        classes_text.append(row['class'].encode('utf8'))
+        classes.append(class_text_to_int(row['class']))
+
+    tf_example = tf.train.Example(features=tf.train.Features(feature={
+        'image/height': dataset_util.int64_feature(height),
+        'image/width': dataset_util.int64_feature(width),
+        'image/filename': dataset_util.bytes_feature(filename),
+        'image/source_id': dataset_util.bytes_feature(filename),
+        'image/encoded': dataset_util.bytes_feature(encoded_jpg),
+        'image/format': dataset_util.bytes_feature(image_format),
+        'image/object/bbox/xmin': dataset_util.float_list_feature(xmins),
+        'image/object/bbox/xmax': dataset_util.float_list_feature(xmaxs),
+        'image/object/bbox/ymin': dataset_util.float_list_feature(ymins),
+        'image/object/bbox/ymax': dataset_util.float_list_feature(ymaxs),
+        'image/object/class/text': dataset_util.bytes_list_feature(classes_text),
+        'image/object/class/label': dataset_util.int64_list_feature(classes),
+    }))
+    return tf_example
+                        
+def generate_tf_records(fold_start=1, fold_end=1):
+    pass
+                    
+                    
+if __name__ == '__main__':
+    
+    preview_index = 4500
+    i = 0
+    for item in read_folds(1,10):
+        i += 1
+        if i == preview_index:
+            break
+    impath = path.join('originalPics', item.image_path)
+    image = cv2.imread(impath)
+    #item.angle, 0, 360, 'red', 5
+    ellipse_box = item.cv2_ellipse_box()
+    cv2.ellipse(image, ellipse_box, (0, 204, 0), 3)
+    p1, p2 = item.cv2_rectangle_box()
+    cv2.rectangle(image, p1, p2, (0, 0, 204), 3)
+    cv2.imshow("preview", image)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
